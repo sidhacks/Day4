4a24bcc69103a3f9d09ef403fda097115f9d99a4
Siddharth Sahu <siddharthsahu.180@gmail.com>
2024-02-20 01:49:31 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+//class Loops{
+//    public static void main (String[] args) {
+//        for (int i = 0; i < 11; i++) {
+//            System.out.println(i);
+//        }
+//    }
+//}
+
+//class Loops{
+//    public static void main (String[] args) {
+//       for (int counter = 0; counter < 100; counter++)
+//           System.out.println("Hello World");
+//    }
+//}
+
+import java.util.*;
+class Loops {
+    public static void main(String[] args) {
+//        for (int counter = 0; counter < 11; counter++)
+//            System.out.println(counter);
+//    }
+//}
+//        int i = 0;
+//        while (i < 11) {
+//            System.out.println(i);
+//            i = i +1;
+//        }
+//    }
+//}
+//
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i * n);
+        }
+    }
+}
